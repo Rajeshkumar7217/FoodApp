ea3bcc3d26de56b1b7e4530489abd94682c66773
Rajesh kumar lenka <124233452+Rajeshkumar7217@users.noreply.github.com>
2025-08-11 12:39:26 +0530

    Delete src directory

--- src/app/model/product-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProductDto } from './product-dto';
-
-describe('ProductDto', () => {
-  it('should create an instance', () => {
-    expect(new ProductDto()).toBeTruthy();
-  });
-});
--- src/app/model/product-dto.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductDto {
-  id: number;
-  name: string;
-  description?: string;
-  price: number;
-  // backend should send base64 string of image bytes (no prefix) or an URL
-  image?: string; 
-}
